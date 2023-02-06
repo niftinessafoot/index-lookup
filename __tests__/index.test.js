197e9ea594fa2479453c0266cdf17ae350f6877a
Matthew Smith <niftinessafoot@gmail.com>
2023-02-06 18:19:26 -0500

    feat: add initial functionality and tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,49 @@
+import { indexLookup } from '../src/index';
+
+describe('IndexLookup', () => {
+  let key;
+  let data;
+  let expectedOutput;
+
+  beforeEach(() => {
+    key = 'id';
+    data = [
+      {
+        id: 'one',
+        name: 'Tacos',
+      },
+      {
+        id: 'two',
+        name: 'Pizza',
+      },
+    ];
+    expectedOutput = ['one', 'two'];
+  });
+
+  it('should return an array of the `id` values', () => {
+    const lookup = indexLookup(key, data);
+    expect(lookup).toEqual(expect.arrayContaining(expectedOutput));
+  });
+
+  it('should return an array of `undefined` if the key does not exist in data', () => {
+    key = 'noop';
+    expectedOutput = [undefined, undefined];
+
+    const warn = jest.spyOn(console, 'warn').mockImplementation(() => true);
+    const lookup = indexLookup(key, data);
+    expect(warn).toHaveBeenCalledWith(
+      `"${key}" is not a key in all supplied objects.`
+    );
+    expect(lookup).toEqual(expect.arrayContaining(expectedOutput));
+  });
+
+  it('should throw an error if `data` is not an array', () => {
+    data = 'one';
+
+    const lookup = () => {
+      indexLookup(key, data);
+    };
+
+    expect(lookup).toThrow('Supplied data is not an array.');
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+type Key = string | number;
+interface IndexLookup {
+  (key: Key, objects: Record<Key, unknown>[]): unknown[];
+}
+
+/**
+ * Generate an index of ids from an array of objects for lookups.
+ *
+ * @param key - A common key in all contained objects. Values form the array elements.
+ * @param objects - An array of object literals, each with a common key property holding a unique value.
+ * @returns An array of values.
+ */
+const indexLookup: IndexLookup = (key, objects) => {
+  if (!Array.isArray(objects)) {
+    throw new Error(`Supplied data is not an array.`);
+  }
+
+  const output = objects.map((obj) => obj[key]);
+
+  if (output.some((obj) => obj === undefined)) {
+    console.warn(`"${key}" is not a key in all supplied objects.`);
+  }
+
+  return output;
+};
+
+export { indexLookup };
